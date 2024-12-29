b5c683e57aeefd873e1b9c92aa58e27c764261f1
ukaleem <mrfaizan726@gmail.com>
2024-12-29 22:58:42 +0500

    user register and login api's done

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express  = require('express');
+const cors = require('cors');
+const userRoutes = require('./routes/user.routes')
+const app = express();
+const connectToDb = require('./db/db'); 
+
+connectToDb();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/users', userRoutes);
+app.get('/login', (req, res) => {
+    res.send('Hello Login!');
+});
+
+app.use('/', userRoutes);
+
+
+module.exports = app;
