497477db97c06077267ff11466d3689313ab3eee
ukaleem <mrfaizan726@gmail.com>
2025-01-11 22:31:22 +0500

    Add remixicon dependency and implement LocationSearchPanel component in Home

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "remixicon/fonts/remixicon.css";
+
+const LocationSearchPanel = () => {
+  return (
+    <div>
+      <div className="flex gap-4 my-4 items-center">
+        <h2 className="h-10 w-14 flex items-center justify-center rounded-full bg-[#eeeeee] text-xl font-semibold">
+          <i className="ri-map-pin-fill"></i>
+        </h2>
+        <h4 className="font-medium">
+          Lakhanwal Kalan P/O Lakhanwal Khas district Gujrat
+        </h4>
+      </div>
+
+
+    </div>
+  );
+};
+
+export default LocationSearchPanel;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import logo from "../assets/Uber_logo_black.png";
 import map from "../assets/uber_map.jpeg";
-import {useGSAP} from '@gsap/react'
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from "../components/LocationSearchPanel";
 
 const Home = () => {
-  const [pickUp, setPickUp] = useState();
-  const [destination, setDestination] = useState();
+  const [pickUp, setPickUp] = useState("");
+  const [destination, setDestination] = useState("");
   const [panelOpen, setPanelOpen] = useState(false);
-
+  const panelRef = useRef(null)
+  const panelCloseRef = useRef(null)
 
   const submitHandler = (e) => {
     e.preventDefault();
-  }
+  };
 
-  
+  useGSAP(()=>{
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+        padding:20,
+        height: '70%'
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity: 1
+      })
+    }
+    else{
+      gsap.to(panelRef.current,{
+        padding:0,
+        height: '0%'
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity: 0
+      })
+    }
+  },[panelOpen, panelCloseRef])
+
   return (
     <div className="h-screen relative">
       <img className="w-16 absolute left-5 top-5" src={logo} alt="Uber logo" />
@@ -22,21 +46,29 @@ const Home = () => {
       </div>
 
       <div className="flex flex-col justify-end h-screen absolute top-0 w-full">
-        <div className="h-[30%] p-5 bg-white">
+        <div className="h-[30%]  p-6 bg-white relative">
+
+          <h5 ref={panelCloseRef} onClick={()=>{setPanelOpen(false)}}
+           className="opacity-0 absolute top-6 right-6 text-2xl">
+            <i className="ri-arrow-down-s-line"></i>
+          </h5>
+
           <h4 className="text-2xl font-semibold">Find a trip</h4>
-          <form onSubmit={(e) =>{
-            submitHandler(e);
-          }}>
+          <form
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
             <input
-            onClick={() => setPanelOpen(true)}
-            value={pickUp}
-            onChange={(e) => setPickUp(e.target.value)}
-              className="bg-[#eeee] px-12 py-2 text-lg rounded-lg w-full mt-3"
+              onClick={() => setPanelOpen(true)}
+              value={pickUp}
+              onChange={(e) => setPickUp(e.target.value)}
+              className="bg-[#eeeeee] px-12 py-2 text-lg rounded-lg w-full mt-3"
               type="text"
               placeholder="Add a pick-up location"
             />
             <input
-            onClick={() => setPanelOpen(true)}
+              onClick={() => setPanelOpen(true)}
               value={destination}
               onChange={(e) => setDestination(e.target.value)}
               className="bg-[#eeee] px-12 py-2 text-lg rounded-lg w-full mt-3"
@@ -46,8 +78,8 @@ const Home = () => {
           </form>
         </div>
 
-        <div className="bg-red-500 h-0">
-
+        <div ref={panelRef} className="bg-white h-0">
+          <LocationSearchPanel />
         </div>
       </div>
     </div>
